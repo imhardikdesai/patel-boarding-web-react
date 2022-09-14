2f5836b77e81a3d190173ae7bf18324fe647d18e
imhardikdesai <hp676913@gmail.com>
2022-09-14 22:00:21 +0530

    added galerBox layout

--- src/components/GalleryBox/GalleryBox.jsx
+++ src/components/GalleryBox/GalleryBox.jsx
@@ -2,34 +2,39 @@ import React from 'react'
 import './GalleryBox.css'
 
 export default function GalleryBox() {
+    const demo = document.getElementById('1');
+    console.log(demo)
+
+    const filterSelection = (id) => {
+        console.log(id);
+
+    }
+    const byDefault = (id) => {
+        console.log(id);
+    }
     return (
         <>
-            <div class="main">
-                <h2 id="galleryHeading" class="shadowHeading text-white text-center mb-2">Photo Gallery</h2>
+            <div className="main">
+                <h2 id="galleryHeading" className="shadowHeading text-white text-center mb-2">Photo Gallery</h2>
                 <div id="myBtnContainer">
-                    <button id="1" class="btn btn-outline-primary m-1" onclick="byDefault(this.id)">All</button>
-                    <button id="2" class="btn btn-outline-primary m-1"
-                        onclick="filterSelection('Campus Experience',this.id)">Campus
-                        Experience</button>
-                    <button id="3" class="btn btn-outline-primary m-1"
-                        onclick="filterSelection('Infrastructure',this.id)">Infrastructure</button>
-                    <button id="4" class="btn btn-outline-primary m-1" onclick="filterSelection('Food',this.id)">Food</button>
-                    <button id="5" class="btn btn-outline-primary m-1" onclick="filterSelection('Gym',this.id)">Gym</button>
-                    <button id="6" class="btn btn-outline-primary m-1"
-                        onclick="filterSelection('Sports',this.id)">Sports</button>
-                    <button id="7" class="btn btn-outline-primary m-1"
-                        onclick="filterSelection('Library',this.id)">Library</button>
+                    <button id='1' className="btn btn-outline-primary m-1" onclick={byDefault(this.id)}>All</button>
+                    <button id="2" className="btn btn-outline-primary m-1" onClick={filterSelection('Campus Experience', this.id)}>Campus Experience</button>
+                    <button id="3" className="btn btn-outline-primary m-1" onClick={filterSelection('Infrastructure', this.id)}>Infrastructure</button>
+                    <button id="4" className="btn btn-outline-primary m-1" onClick={filterSelection('Food', this.id)}>Food</button>
+                    <button id="5" className="btn btn-outline-primary m-1" onClick={filterSelection('Gym', this.id)}>Gym</button>
+                    <button id="6" className="btn btn-outline-primary m-1" onClick={filterSelection('Sports', this.id)}>Sports</button>
+                    <button id="7" className="btn btn-outline-primary m-1" onClick={filterSelection('Library', this.id)}>Library</button>
                 </div>
 
-                <div class="container">
+                <div className="container">
 
-                    <div id="gallery" class="row justify-content-center">
+                    <div id="gallery" className="row justify-content-center">
 
-                        {/* <div class="col-md-3 m-2">
-                                <div class="card">
-                                <img class="card-img-top img-thumbnail" src="Assets/Food (2).jpeg" alt="Card image cap">
-                                    <div class="card-body">
-                                        <p class="card-text">Campus Experience</p>
+                        {/* <div className="col-md-3 m-2">
+                                <div className="card">
+                                <img className="card-img-top img-thumbnail" src="Assets/Food (2).jpeg" alt="Card image cap">
+                                    <div className="card-body">
+                                        <p className="card-text">Campus Experience</p>
                                     </div>
                                 </div>
                              </div>  */}
--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner/Banner'
+import GalleryBox from '../components/GalleryBox/GalleryBox'
 
 export default function GalleryPage() {
     return (
         <>
            <Banner/>
+           <GalleryBox/>
         </>
     )
 }
